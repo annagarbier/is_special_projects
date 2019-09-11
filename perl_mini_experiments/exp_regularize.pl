470f78bd412f31a1d2e2b3c909b96ab5502d4db9
annagarbier <annagarbier@gmail.com>
2019-09-11 12:26:30 -0400

    Update exp_regularize.pl

--- perl_mini_experiments/exp_regularize.pl
+++ perl_mini_experiments/exp_regularize.pl
@@ -9,13 +9,12 @@ use autodie; # Die if problem reading or writing a file
 my $file_in = path("text_irregular_sentence.txt");
 my $file_in_handle = $file_in->openr_utf8();
 
-# Read in the entire contents of file
+# Read in the entire contents of file and print
 my $content = $file_in->slurp_utf8();
 print("\nOLD: ".$content);
 
-# Make replacements
+# Make replacements and print
 $content =~ s/feet/foots/g;
 $content =~ s/went/goed/g;
 $content =~ s/took/taked/g;
-
 print("NEW: ".$content."\n");
